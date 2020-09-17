f94602c163748c15d86a5f53bdbac68a0055612e
raisulislampiaus <raisulislam714@gmail.com>
2020-09-17 13:29:19 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const bp      = require('body-parser');
-
+const signup = require('./controllers/signup');
 const app     = express();
+
+app.use(bp.json());
+
+app.use(signup);
 
 
 const __port = process.env.PORT || 5000;
--- src/utils/db.js
+++ src/utils/db.js
@@ -5,7 +5,10 @@ const db = new Sequelize(CONNECTION_STRING);
 
 const User = db.define('users',{
     name:Sequelize.TEXT,
-    email:Sequelize.TEXT,
+    email:{
+        type:Sequelize.TEXT,
+        unique:true
+    },
     password:Sequelize.TEXT
 });
 
@@ -13,6 +16,11 @@ const Direction = db.define('direction',{
     destination:Sequelize.TEXT,
     hash:Sequelize.TEXT
 });
+
+db.sync()
+    .then(e=>{
+        console.log(`Database Synced`)
+    }).catch(e=>console.log(e.message));
 
 module.exports = {
     db,User,Direction
