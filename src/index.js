dbdcb4e0fe16edb3800e5fed9f173a1796463355
raisulislampiaus <raisulislam714@gmail.com>
2020-09-22 23:56:20 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bp      = require('body-parser');
 const signup = require('./controllers/signup');
+const login = require('./controllers/login');
 const app     = express();
 
 app.use(bp.json());
 
 app.use(signup);
+app.use(login);
 
 
 const __port = process.env.PORT || 5000;
